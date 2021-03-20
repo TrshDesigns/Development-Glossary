f8cb5a5fcd333eaadbd28aff18cd6ac8f6883f7a
TrshDesigns <trshdesigns@outlook.com>
2021-03-20 18:42:20 -0300

    testing srvice worker

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ var cacheName = 'v1';
 
 // Default files to always cache
 var cacheFiles = [
+	'/',
 	'/index.html',
 	'./css/style.css',
 	'https://fonts.googleapis.com/css2?family=Fascinate&display=swap'
