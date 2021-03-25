4df18285deec970c965ced7707f5bec03fbaa466
TrshDesigns <trshdesigns@outlook.com>
2021-03-24 22:28:35 -0300

    testing

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,22 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
-workbox.routing.registerRoute(
-	({request}) => request.destination === 'image',
-	new workbox.strategies.NetworkFirst()
-	
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+import {CacheFirst} from 'workbox-strategies';
+import {ExpirationPlugin} from 'workbox-expiration';
+import {registerRoute} from 'workbox-routing';
+
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst({
+	cacheName: 'images',
+	plugins: [
+	  new CacheableResponsePlugin({
+		statuses: [0, 200],
+	  }),
+	  new ExpirationPlugin({
+		maxEntries: 60,
+		maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+	  }),
+	],
+  }),
 );
