fcfd0bed5d7387dcebdc353621e01959fc787f88
TrshDesigns <trshdesigns@outlook.com>
2021-03-24 22:19:16 -0300

    testing

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,3 @@
-import {registerRoute} from 'workbox-routing';
-import {CacheFirst} from 'workbox-strategies';
-import {CacheableResponsePlugin} from 'workbox-cacheable-response';
-
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
@@ -10,15 +5,3 @@ workbox.routing.registerRoute(
 	new workbox.strategies.NetworkFirst()
 	
 );
-registerRoute(
-	({url}) => url.origin === 'https://sad-mcclintock-8c1c7b.netlify.app' &&
-			   url.pathname.startsWith('/images/'),
-	new CacheFirst({
-	  cacheName: 'image-cache',
-	  plugins: [
-		new CacheableResponsePlugin({
-		  statuses: [0, 200],
-		})
-	  ]
-	})
-  );
