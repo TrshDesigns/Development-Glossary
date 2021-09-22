349a3be673a1024ef6801a488743b4596fc14afe
TrshDesigns <trshdesigns@outlook.com>
2021-09-22 11:27:11 -0300

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
+	({request}) => request.destination === 'html',
+	new workbox.strategies.NetworkFirst()
+	
+);
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'css',
+	new workbox.strategies.NetworkFirst()
+	
+);
+workbox.routing.registerRoute(
 	({request}) => request.destination === 'image',
 	new workbox.strategies.NetworkFirst()
 	
